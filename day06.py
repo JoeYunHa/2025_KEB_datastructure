c36c96e8e7ca77c73442a94ee109f1ff6644500e
JoeYunHa <12201806@inha.edu>
2025-02-17 15:56:32 +0900

    v1.0 day06 array with numpy

--- day06.py
+++ day06.py
@@ -1,28 +1,6 @@
-fibonacci = [0,1]
+import numpy as np
 
-def fibonacci_recursion(n) -> int:
-    if n <=1:
-        return fibonacci[n]
-    else:
-        if len(fibonacci) > n:
-            return fibonacci[n]
-        else:
-            fibonacci.append(fibonacci_recursion(n-2) + fibonacci_recursion(n-1))
-            return fibonacci[n]
+narray = np.array([1,3,2,9])
+print(type(narray))
 
-memo = dict()
-def fibonacci_memo(n) -> int:
-    if n in memo:
-        return memo[n]
-    elif n <= 1:
-        return n
-    else:
-        memo[n] = fibonacci_memo(n-2) + fibonacci_memo(n-1)
-        return memo[n]
-
-
-
-num = int(input())
-print(fibonacci_recursion(num))
-print(fibonacci_memo(num))
-
+print(narray[0], narray[1], narray[2],narray[3])
