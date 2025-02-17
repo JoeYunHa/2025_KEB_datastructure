4e3ce0de0804be27e416561eb90dde3c42098e74
JoeYunHa <12201806@inha.edu>
2025-02-17 15:09:12 +0900

    v0.2 Add other memoization method

--- day06.py
+++ day06.py
@@ -10,7 +10,19 @@ def fibonacci_recursion(n) -> int:
             fibonacci.append(fibonacci_recursion(n-2) + fibonacci_recursion(n-1))
             return fibonacci[n]
 
+memo = dict()
+def fibonacci_memo(n) -> int:
+    if n in memo:
+        return memo[n]
+    elif n <= 1:
+        return n
+    else:
+        memo[n] = fibonacci_memo(n-2) + fibonacci_memo(n-1)
+        return memo[n]
+
+
+
 num = int(input())
 print(fibonacci_recursion(num))
-
+print(fibonacci_memo(num))
 
