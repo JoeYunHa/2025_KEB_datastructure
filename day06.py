66b47aa00dbda97e444a035dad5aa2d90c80aab5
JoeYunHa <12201806@inha.edu>
2025-02-17 14:53:06 +0900

    v0.1 Define fibonacci with momoization

--- day06.py
+++ day06.py
@@ -1,35 +1,16 @@
-def print_poly(f_x, t_x) -> str:
-    #term = len(f_x) - 1
-    poly_expression = "f(x) = "
+fibonacci = [0,1]
 
-    for i in range(len(fx)):
-        coefficient = f_x[i]
-        term = t_x[i]
+def fibonacci_recursion(n) -> int:
+    if n <=1:
+        return fibonacci[n]
+    else:
+        if len(fibonacci) > n:
+            return fibonacci[n]
+        else:
+            fibonacci.append(fibonacci_recursion(n-2) + fibonacci_recursion(n-1))
+            return fibonacci[n]
 
-        if coefficient >= 0 and i != 0: # feat. csh
-                poly_expression = poly_expression + "+"
-        poly_expression = poly_expression + f'{coefficient}x^{term} '
-        #term = term - 1
-
-    return poly_expression
-
-
-def calculation_poly(x_value, f_x, t_x) -> int:
-    return_value = 0
-    # term = len(f_x) - 1
-
-    for i in range(len(fx)):
-        coefficient = f_x[i]
-        term = t_x[i]
-        return_value += coefficient * pow(x_value, term)
-        # term = term - 1
-
-    return return_value
+num = int(input())
+print(fibonacci_recursion(num))
 
 
-fx = [2, 5, -9, 11]
-tx = [20, 7, 2, 0]
-
-if __name__ == "__main__":
-    print(print_poly(fx, tx))
-    print(calculation_poly(int(input("x 값 : ")), fx, tx))
