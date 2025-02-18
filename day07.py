e75c246eac4cc4e19d88d2756acd8f88ae95b458
JoeYunHa <12201806@inha.edu>
2025-02-18 17:07:11 +0900

    Modify Assignment day07

--- day07.py
+++ day07.py
@@ -1,20 +1,9 @@
 def is_queue_full() :
     global size,queue,front,rear
-    if rear != (size-1):
-        return False
-    elif (front == -1) and (rear == size-1):
+    if rear - front == size and rear == size - 1:
         return True
-    else: # left shift
-        for i in range(front+1,size):
-            queue[i-1] = queue[i]
-            queue[i] = None
-        front = front - 1
-        rear = rear - 1
+    else:
         return False
-    # if rear - front == size and rear == size - 1:
-    #     return True
-    # else:
-    #     return False
 
 def is_queue_empty() :
     global size, queue, front, rear
@@ -28,7 +17,7 @@ def en_queue(data) :
     if is_queue_full():
         print("큐가 꽉 찼습니다.")
         return
-    rear += 1
+    rear = (rear + 1) % size
     queue[rear] = data
 
 def de_queue() :
@@ -36,7 +25,7 @@ def de_queue() :
     if is_queue_empty():
         print("큐가 비었습니다.")
         return None
-    front += 1
+    front = (front + 1) % size
     data = queue[front]
     queue[front] = None
     return data
