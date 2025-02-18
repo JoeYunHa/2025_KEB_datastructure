80b373d8a316b073d9aba229b83b0b8cf5545bad
JoeYunHa <12201806@inha.edu>
2025-02-18 15:29:31 +0900

    Add Queue class

--- day07.py
+++ day07.py
@@ -1,64 +1,45 @@
 class Node:
-    def __init__(self, data, next=None):
+    def __init__(self,data,next=None):
         self.data = data
         self.next = next
 
-class LinkedList:
+class Queue:
     def __init__(self):
-        self.head = None
+        self.front = None
+        self.rear = None
+        self._size = 0
 
-    def append(self, data):
-        if not self.head:
-            self.head = Node(data)
-            return
-        current = self.head
-        while current.next:  # if next node exist
-            current = current.next  # move
-        current.next = Node(data)
+    def enqueue(self, data):
+        self._size += 1
+        node = Node(data)
 
-    def search(self, target) -> bool:
-        current = self.head
-        while current.next:
-            if current.data == target:
-                return True
-            else:
-                current = current.next
-        return False
+        if self.rear is None:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.next = node
+            self.rear = node
 
-    def remove(self, target):
-        if self.head.data == target:
-            self.head = self.head.next
-            return
-        current = self.head.next
-        prev = self.head
-        while current.data is not target:
-                prev = current
-                current = current.next
-        prev.next = current.next
+    def dequeue(self):
+        if self.front is None:
+            raise IndexError("pop from empty queue")
+        self._size -= 1
+        temp = self.front.data
+        self.front = self.front.next
+        if self.front is None:
+            self.rear = None
+        return temp
 
-    def __str__(self):
-        node = self.head
-        while node is not None:
-            print(node.data)
-            node = node.next
-        return "end"
-
-
-
+    def size(self)->int:
+        return self._size
 
 if __name__ == "__main__":
-    l = LinkedList()
-    l.append(7)
-    l.append(-11)
-    l.append(8)
-    l.remove(8)
-    print(l)
-    # l = LinkedList()
-    # i = 0
-    # while i < 20:
-    #     n = random.randint(1,20)
-    #     l.append(n)
-    #     print(n, end=' ')
-    #     i = i + 1
-    # # print(l)
-    # print(l.search(10))
+    q = Queue()
+    q.enqueue(7)
+    q.enqueue(-11)
+    q.enqueue(8)
+    print(q.size())
+    print(q.dequeue())
+
+
+
