b87370856452d4636fefdb8c7e39f5b76904b292
JoeYunHa <12201806@inha.edu>
2025-02-21 10:23:30 +0900

    v4.7 day10 sort update

--- day10.py
+++ day10.py
@@ -1,6 +1,6 @@
 def bubble_sort(l):
     for i in range(len(l) - 1):
-        for j in range(len(l)-1):
+        for j in range(len(l)- 1 -i):
             if l[j] > l[j+1]:
                 l[j], l[j+1] = l[j+1] , l[j] # swap
     return l
