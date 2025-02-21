d87fa5f761c39dbe438105573d370ab0b86ce57d
JoeYunHa <12201806@inha.edu>
2025-02-21 13:19:34 +0900

    v5.0 sort

--- day10.py
+++ day10.py
@@ -1,3 +1,27 @@
+import time
+import random
+
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f"실행 시간 : {e - s } s")
+        return r
+    return wrapper
+
+@time_decorator
+def insertion_sort(l):
+    for i in range(1,len(l)):
+        value = l[i]
+        while i > 0 and l[i-1] > value:
+            l[i] = l[i-1] # ascend
+            i = i - 1
+            print(i, end=' ')
+        l[i] = value
+    return l
+
+@time_decorator
 def bubble_sort(l):
     for i in range(len(l) - 1):
         no_swap = True
@@ -10,6 +34,16 @@ def bubble_sort(l):
                 return l
     return l
 
-print(bubble_sort([8,-11,9,1]))
-print(bubble_sort([-11,1,8,9]))
+
+list1 = [random.randint(1,10000) for _ in range(10000)]
+list2 = list1.copy()
+
+bubble_sort(list1)
+insertion_sort(list2)
+
+# print(bubble_sort([8,-11,9,1]))
+# print(bubble_sort([-11,1,8,9]))
+#
+# print(insertion_sort([8,-11,9,1]))
+# print(insertion_sort([-11,1,8,9]))
 
